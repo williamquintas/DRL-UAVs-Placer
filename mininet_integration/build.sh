0cbeb7971e1eb1ecd0f8d196ab0a3051bd34d4b3
William Melo <william.quintas@gmail.com>
2022-06-08 19:30:36 -0300

    #33: Fixes build and deploy shell scripts

--- mininet_integration/build.sh
+++ mininet_integration/build.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 rm -rf dist
-
 mkdir dist
-cp -R ../controllers ./dist
-cp -R ../gym_envs ./dist
-cp -R ../utils ./dist
 
-mkdir dist/data
-cp -R ./data ./dist
+cp -R ../controllers ../gym_envs ../utils ./data ../Pipfile ./*.py ./deploy.sh ./dist
 
-cp ../Pipfile ./dist
-cp ./*.py ./dist
-cp ./deploy.sh ./dist
-
-pipenv run 3to2 dist -w
+pipenv run 3to2 dist -w -n --no-diffs
 
 rm -rf ../scripts/dist
 cp -R dist ../scripts
