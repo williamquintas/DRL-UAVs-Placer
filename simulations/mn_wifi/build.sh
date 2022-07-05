a4bc9e5e66d383e52382dc37aa5a0714d4e7ad4a
William Melo <william.quintas@gmail.com>
2022-07-05 01:34:17 -0300

    #45: Moves Mininet-Wifi simulations to own directory

--- simulations/mn_wifi/build.sh
+++ simulations/mn_wifi/build.sh
@@ -3,11 +3,8 @@ rm -rf dist
 mkdir dist
 
 cp -R ./get_weighted_center_of_mass_data ./dist
-cp -R ../controllers ../gym_envs ../utils ./data ../Pipfile ./deploy.sh ./dist
+cp -R ../../controllers ../../gym_envs ../../utils ./data ../../Pipfile ./deploy.sh ./dist
 
 pipenv run 3to2 dist -w -n --no-diffs
-
-rm -rf ../scripts/dist
-cp -R dist ../scripts
 
 
